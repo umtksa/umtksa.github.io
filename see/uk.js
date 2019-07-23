317c303e15f104934c33b3d970eacafd7c708d39
umtksa <umtksa@gmail.com>
2019-07-23 20:58:44 +0300

    Update uk.js

--- see/uk.js
+++ see/uk.js
@@ -1,12 +1,4 @@
-var count = parseInt(localStorage.getItem('count')) || 0;
-
-simply.on('singleClick', function(e) {
-  if (e.button === 'up') {
-    simply.subtitle(++count);
-  } else if (e.button === 'down') {
-    simply.subtitle(--count);
-  }
-  localStorage.setItem('count', count);
+ajax({ url: 'shortcuts://open-shortcut?name=update' }, function(data){
+  var headline = data.match(/<h1>(.*?)<\/h1>/)[1];
+  simply.title(headline);
 });
-
-simply.text({ title: 'Counter', subtitle: count });
